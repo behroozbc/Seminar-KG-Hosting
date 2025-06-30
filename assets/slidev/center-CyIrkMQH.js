253870e497583de6361538f6862ac1dc014a1ae9
behroozbc <behroozbc@outlook.com>
2025-06-30 21:56:53 +0000

    Deploying to gh-pages from  @ 0506f3e466ea12a46751d7b7e9d1246f10866f8e 🚀

--- assets/slidev/center-CyIrkMQH.js
+++
@@ -1,1 +0,0 @@
-import{f as t,o,g as s,j as c}from"../modules/vue-D0HQ-a_i.js";import{_ as r}from"../index-CQhj7Kt0.js";const a={},n={class:"slidev-layout center h-full grid place-content-center"},l={class:"my-auto"};function d(e,_){return o(),t("div",n,[s("div",l,[c(e.$slots,"default")])])}const m=r(a,[["render",d]]);export{m as I};
